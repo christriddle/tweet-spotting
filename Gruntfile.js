6fabeca095a032c300f2f7c7fd9e6b7f2fd55e98
Chris Ridde <criddle@opentable.com>
2014-11-07 12:19:19 +0000

    Added build and linting

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+    "use strict";
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'lib/**/*.js'
+            ]
+        }
+    });
+
+    // Automatically load in all Grunt npm tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.registerTask('default', 'build');
+    grunt.registerTask('build', ['jshint']);
+    grunt.registerTask('test', ['jshint']);
+};
